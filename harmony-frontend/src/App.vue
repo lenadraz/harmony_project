86510e1656f300f4ce934087aee008dca88bb313
lenadraz <128379882+lenadraz@users.noreply.github.com>
2026-01-21 16:41:57 +0200

    Update App.vue

--- harmony-frontend/src/App.vue
+++ harmony-frontend/src/App.vue
@@ -1,3 +1,6 @@
 <template>
+   <!-- Root application component.
+       It only renders the current route using Vue Router.
+       All actual pages (Login, Matches, Profile, etc.) are injected here via <router-view>. -->
   <router-view />
 </template>
